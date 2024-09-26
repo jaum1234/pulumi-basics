15bee025ed7e0fe6f07192e4ba6809302a1f4479
joao coura <joaovitorsouzacoura@gmai.com>
2024-09-26 06:50:40 -0300

    :sparkles: feat: criando um BucketObject com um arquivo index.html.

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
-// Export the name of the bucket
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("./index.html")
+});
+
 export const bucketName = bucket.id;
