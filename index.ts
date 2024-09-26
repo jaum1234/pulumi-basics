70b0cdfbcc853c772fbde1cace4581f9082c8275
joao coura <joaovitorsouzacoura@gmai.com>
2024-09-26 06:53:20 -0300

    :sparkles: feat: adicionando o BucketObject como website do bucket.

--- index.ts
+++ index.ts
@@ -2,7 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket", {
+    website: {
+        indexDocument: "index.html"
+    }
+});
 
 const bucketObject = new aws.s3.BucketObject("index.html", {
     bucket: bucket.id,
