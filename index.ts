2fd02e703d3239cb029e4bb5085815b5bc18f11d
joao coura <joaovitorsouzacoura@gmai.com>
2024-09-26 07:10:53 -0300

    :sparkles: feat: configurando acesso público e controles de proprietário do BucketObject.

--- index.ts
+++ index.ts
@@ -8,9 +8,24 @@ const bucket = new aws.s3.Bucket("my-bucket", {
     }
 });
 
-const bucketObject = new aws.s3.BucketObject("index.html", {
+const controls = new aws.s3.BucketOwnershipControls("ownership-controls", {
     bucket: bucket.id,
-    source: new pulumi.asset.FileAsset("./index.html")
+    rule: {
+        objectOwnership: "ObjectWriter"
+    }
 });
 
+const block = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false
+});
+
+const object = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("./index.html"),
+    contentType: "text/html",
+    acl: "public-read"
+}, { dependsOn: [block, controls] });
+
 export const bucketName = bucket.id;
+export const endpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`
